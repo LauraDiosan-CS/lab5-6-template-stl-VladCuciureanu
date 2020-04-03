b4a3667251d1660a3787a682028164e64d0b517c
Vlad Cuciureanu <55031827+VladCuciureanu@users.noreply.github.com>
2020-04-03 15:06:52 +0300

    v103 - Removed unused code from Repo

--- RepoSTLTemplate.cpp
+++ RepoSTLTemplate.cpp
@@ -1,18 +1,1 @@
 #include "RepoSTLTemplate.h"
-
-/*
-template <class T>
-void RepoSTLTemplate<T>::updateElem(T& p, int cakeId, const char* name, const char* ingredients, double price)
-{
-	for (int i = 0; i < elem.size(); i++)
-	{
-		if (elem[i] == p)
-		{
-			elem[i].setId(cakeId);
-			elem[i].setName(name);
-			elem[i].setIngredients(ingredients);
-			elem[i].setPrice(price);
-		}
-	}
-
-}*/
